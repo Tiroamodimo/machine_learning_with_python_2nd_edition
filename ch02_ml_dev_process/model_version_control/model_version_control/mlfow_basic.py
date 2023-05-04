6e19244dd5c72ad0d9f0a51a20e381564e482c4a
letlhogile05 <letlhogile@gometroapp.com>
2023-05-04 14:30:05 +0200

    failed to run prophet. moving on

--- ch02_ml_dev_process/model_version_control/model_version_control/mlfow_basic.py
+++ ch02_ml_dev_process/model_version_control/model_version_control/mlfow_basic.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     log_param("param1", randint(0, 100))
 
     # Log a metric; metrics can be updated throughout the run
-    log_metric("foo", random())
-    log_metric("foo", random() + 1)
-    log_metric("foo", random() + 2)
+    log_metric("foo1", random())
+    log_metric("foo2", random() + 1)
+    log_metric("foo3", random() + 2)
 
     # Log an artifact (output file)
     if not os.path.exists("outputs"):
